f683f47658bc1120bdc29c915a73b358f01483ea
fjx <1300448152@qq.com>
2024-05-15 16:26:20 +0800

    2

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,66 +1,68 @@
-import { nav } from './config/nav'
-import { sidebar } from './config/sidebar'
-import { PluginTable } from './plugin'
-import type MarkdownIt from 'markdown-it'
+import { nav } from './config/nav';
+import { sidebar } from './config/sidebar';
+import { PluginTable } from './plugin';
+import type MarkdownIt from 'markdown-it';
+
+console.log(sidebar);
 
 /**
  * 更多配置项参考：
- * 
+ *
  * @see app-configs https://vitepress.vuejs.org/config/app-configs.html
  */
 export default {
-  title: 'vitepress-template',
-  /**
-   * 是否显示最后更新时间
-   *
-   * @see last-updated https://vitepress.vuejs.org/guide/theme-last-updated#last-updated
-   */
-  lastUpdated: true,
-  /**
-   * 缓存目录
-   *
-   * @see cacheDir https://vitepress.vuejs.org/config/app-configs#cachedir
-   */
-  cacheDir: '../../node_modules',
-  /**
-   * 主题配置
-   *
-   * @see theme-config https://vitepress.vuejs.org/guide/migration-from-vitepress-0#theme-config
-   */
-  themeConfig: {
+    title: 'vitepress-template',
     /**
-     * 最后更新时间的文案显示
+     * 是否显示最后更新时间
      *
-     * @see lastUpdatedText https://vitepress.vuejs.org/config/theme-configs#lastupdatedtext
+     * @see last-updated https://vitepress.vuejs.org/guide/theme-last-updated#last-updated
      */
-    lastUpdatedText: '最后更新时间',
+    lastUpdated: true,
     /**
-     * 配置导航栏图表
+     * 缓存目录
      *
-     * @see socialLinks https://vitepress.vuejs.org/config/theme-configs#sociallinks
+     * @see cacheDir https://vitepress.vuejs.org/config/app-configs#cachedir
      */
-    socialLinks: [
-      {
-        icon: 'github',
-        link: 'https://github.com/Tyh2001/vitepress-template'
-      }
-    ],
-    nav,
-    sidebar
-  },
-  /**
-   * 自定义 markdown 解析器
-   *
-   * @see markdown https://vitepress.vuejs.org/config/app-configs#markdown
-   */
-  markdown: {
+    cacheDir: '../../node_modules',
     /**
-     * 配置 Markdown-it 实例
+     * 主题配置
      *
-     * @param { Object } md markdown 实例
+     * @see theme-config https://vitepress.vuejs.org/guide/migration-from-vitepress-0#theme-config
      */
-    config: (md: MarkdownIt): void => {
-      md.use(PluginTable)
-    }
-  }
-}
+    themeConfig: {
+        /**
+         * 最后更新时间的文案显示
+         *
+         * @see lastUpdatedText https://vitepress.vuejs.org/config/theme-configs#lastupdatedtext
+         */
+        lastUpdatedText: '最后更新时间',
+        /**
+         * 配置导航栏图表
+         *
+         * @see socialLinks https://vitepress.vuejs.org/config/theme-configs#sociallinks
+         */
+        socialLinks: [
+            {
+                icon: 'github',
+                link: 'https://github.com/Tyh2001/vitepress-template',
+            },
+        ],
+        nav,
+        sidebar,
+    },
+    /**
+     * 自定义 markdown 解析器
+     *
+     * @see markdown https://vitepress.vuejs.org/config/app-configs#markdown
+     */
+    markdown: {
+        /**
+         * 配置 Markdown-it 实例
+         *
+         * @param { Object } md markdown 实例
+         */
+        config: (md: MarkdownIt): void => {
+            md.use(PluginTable);
+        },
+    },
+};
--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -1,18 +1,66 @@
-/**
- * 侧边栏菜单
- *
- * @see sidebar https://vitepress.vuejs.org/guide/theme-sidebar#sidebar
- */
+import * as fs from 'fs';
+import * as path from 'path';
+
+const docsPath = path.resolve(process.cwd(), 'docs');
+// console.log(docsPath);
+
+// const files = fs.readdirSync(docsPath);
+// console.log(files);
+
+// for (const file of files) {
+//     const p = path.join(docsPath, file);
+//     const stat = fs.statSync(p);
+//     if (stat.isDirectory()) {
+//         console.log(file, p);
+//     }
+// }
+
+interface SidebarItem {
+    text: string;
+    link: string;
+    collapsible?: boolean;
+    items?: SidebarItem[];
+}
+
+function fun(root: string) {
+    debugger;
+    function toList(p: string, prefix = '/docs') {
+        const arr = [] as SidebarItem[];
+        const files = fs.readdirSync(p);
+        for (const file of files) {
+            const tmp = { text: file, link: prefix + '/' + file } as SidebarItem;
+            const p1 = path.join(p, file);
+            const stat = fs.statSync(p1);
+            if (stat.isDirectory()) {
+                tmp.collapsible = true;
+                tmp.items = toList(p1, prefix + '/' + file);
+            } else {
+                if (tmp.text.endsWith('.md')) {
+                    tmp.text = tmp.text.replace('.md', '');
+                }
+                tmp.link = tmp.link.replace('.md', '');
+            }
+            arr.push(tmp);
+        }
+        return arr;
+    }
+    const list = toList(root);
+    return list;
+}
+
+console.log(fun(docsPath));
+
 export const sidebar = {
-    '/docs/': [
-        {
-            text: '快速上手',
-            collapsible: false,
-            link: '/docs/hello',
-            items: [
-                { text: 'hello', link: '/docs/hello' },
-                { text: '欢迎使用', link: '/docs/welcome' },
-            ],
-        },
-    ],
+    // '/docs/': [
+    //     {
+    //         text: '快速上手',
+    //         collapsible: false,
+    //         link: '/docs/hello',
+    //         items: [
+    //             { text: 'hello', link: '/docs/hello' },
+    //             { text: '欢迎使用', link: '/docs/welcome' },
+    //         ],
+    //     },
+    // ],
+    '/docs/': fun(docsPath),
 };
