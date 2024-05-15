7b330976851578a8b3cb9b7aa5a300a7f3b43292
fjx <1300448152@qq.com>
2024-05-15 15:35:45 +0800

    1

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -4,15 +4,15 @@
  * @see sidebar https://vitepress.vuejs.org/guide/theme-sidebar#sidebar
  */
 export const sidebar = {
-  '/docs/': [
-    {
-      text: '快速上手',
-      collapsible: true,
-      link: '/docs/hello',
-      items: [
-        { text: 'hello', link: '/docs/hello' },
-        { text: '欢迎使用', link: '/docs/welcome' }
-      ]
-    }
-  ]
-}
+    '/docs/': [
+        {
+            text: '快速上手',
+            collapsible: false,
+            link: '/docs/hello',
+            items: [
+                { text: 'hello', link: '/docs/hello' },
+                { text: '欢迎使用', link: '/docs/welcome' },
+            ],
+        },
+    ],
+};
